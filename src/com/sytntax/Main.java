331b529cc42c8053e41f4c53d852cc1beead7b02
BurjuYuksel <burcuyuksel.tgmail.com>
2021-11-03 20:14:18 -0400

    the blue ferrari car better then the lambo i created in stashing,if you wan to see that let me know

--- src/com/sytntax/Main.java
+++ src/com/sytntax/Main.java
@@ -7,5 +7,12 @@ public class Main {
 
         car newcar=new car();
 
+        newcar.name="Feerarriii";
+        newcar.color="blue";
+        newcar.doors=3;
+        newcar.engineCapacity="69887cc";
+
+        System.out.println(newcar.color);
+        
     }
 }
